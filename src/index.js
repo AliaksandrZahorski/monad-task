c4dc40c23c5035c83e9725bdf1680332e53bd467
Aliaksandr_Zahorski <aliaksandr_zahorski@epam.com>
2017-12-29 17:39:13 +0300

    solution is added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function canReach(startPos, finalPos, steps) {
-  // your solution here
+  const uniq = arr => {
+    const hashTable = new Map();
+  	return arr.filter(function (el) {
+  		const key = JSON.stringify(el);
+  		return (hashTable.has(key) ? false : hashTable.set(key, null));
+  	});
+  }
+
+  const inBoard = n => (n > 0 && n < 9) ? true : false;
+
+  const singleMove = cell => {
+    const x = cell[0];
+    const y = cell[1];
+    const arr = [
+      [x - 1, y + 2],
+      [x + 1, y + 2],
+      [x + 2, y + 1],
+      [x + 2, y - 1],
+      [x + 1, y - 2],
+      [x - 1, y - 2],
+      [x - 2, y - 1],
+      [x - 2, y + 1],
+    ];
+    return arr.filter(el => inBoard(el[0]) && inBoard(el[1]));
+  }
+
+  const move = arr => uniq(arr.reduce((acc, curr) => [...acc, ...singleMove(curr)], []));
+
+  const canReach2 = (start, end, step) => {
+    let result = [];
+    result.push(start);
+    for (let i = 0; i < step; i++) {
+      result = move(result);
+    }
+
+    return !!result.filter(el => el[0] === end[0] && el[1] === end[1]).length;
+  }
+
+  return canReach2(startPos, finalPos, steps);
 }
